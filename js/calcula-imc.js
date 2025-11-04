b1f4364c56c225990dc8c2165633b32fbdeb2c4a
Tiago <tiago.amancio@escola.pr.gov.br>
2025-11-04 10:06:10 -0300

    9

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -38,7 +38,7 @@ for (var i = 0; i < pacientes.length; i++) {
     }
 }
 
-function calculaImc (peso, altura);
+function calculaImc (peso, altura){
     var imc = 0;
     var imc = peso / (altura * altura);
     return imc.toFixed(2);
