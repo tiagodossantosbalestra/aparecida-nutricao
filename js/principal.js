15e135892181d1112046c8bf303468c91c29392e
Tiago <tiago.amancio@escola.pr.gov.br>
2025-11-04 08:57:02 -0300

    d

--- js/principal.js
+++ js/principal.js
@@ -18,14 +18,14 @@ for (var i = 0; i < pacientes.length; i++) {
     var pesoEhValido = true;
     var alturaEhValida = true;
 
-    if (peso <= 0 || peso >= 1000) {
+    if (peso >= 0 || peso <= 1000) {
         console.log("Peso inválido");
         pesoEhValido = false;
         tdImc.textContent = "Peso inválido!";
        paciente.classList.add("PACIENTE-INVÁLIDO");
     }
 
-    if (altura <= 0 || altura >= 3.00) {
+    if (altura >= 0 || altura <= 3.00) {
         console.log("Altura inválida!");
         alturaEhValida = false;
         tdImc.textContent = "Altura Inválida!";
