a9859ef0029b5c033421942b2d93efd476e7f91e
Tiago <tiago.amancio@escola.pr.gov.br>
2025-11-11 08:57:43 -0300

    88

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -1,12 +1,10 @@
 var titulo = document.querySelector("h1");
-console.log(titulo);
-console.log(titulo.textContent);
-titulo.textContent = ("Tiago Nutricionista");
+titulo.textContent = ("Vlá Nutrição");
 
 var pacientes = document.querySelectorAll(".paciente");
-
-for (var i = 0; i < pacientes.length; i++) {
+for (var i = 0; i < pacientes.length; i++) { // abre chaves do for 
     var paciente = pacientes[i];
+
     var tdPeso = paciente.querySelector(".info-peso");
     var peso = tdPeso.textContent;
 
@@ -20,21 +18,21 @@ for (var i = 0; i < pacientes.length; i++) {
 
     if (peso <= 0 || peso >= 1000) {
         pesoEhValido = false;
-        tdImc.textContent = "Peso inválido!";
-       paciente.classList.add("paciente-invalido");
+        tdImc.textContent = "Peso Inválido";
+        paciente.classList.add("paciente-invalido");
     }
 
     if (altura <= 0 || altura >= 3.00) {
         alturaEhValida = false;
-        tdImc.textContent = "Altura Inválida!";
-        paciente.classList.add("paciente-invalido"");
+        tdImc.textContent = "Altura Inválida";
+        paciente.classList.add("paciente-invalido");
     }
 
-    if (alturaEhValida && pesoEhValido) {
-        var imc = calculaImc(peso,altura);
-        tdImc.textContent = imc.toFixed(2);
+  if (alturaEhValida && pesoEhValido) {
+        var imc = calculaImc(peso, altura);
+        tdImc.textContent = imc;
     }
-}
+} 
 
 function calculaImc (peso, altura){
     var imc = 0;
